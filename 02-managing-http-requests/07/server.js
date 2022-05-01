db766ce601f2c1aae15484e1005aa47ea924ef39
bethsilverstein <beth.silverstein@tradeshift.com>
2022-05-01 16:38:56 -0500

    Configure for HTTPS

---
+++ 02-managing-http-requests/07/server.js
@@ -0,0 +1,29 @@
+const http = require('https');
+const services = require('../../services');
+const url = require('url');
+const jsonBody = require("body/json");
+const fs = require('fs');
+
+const server = http.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+});
+server.on('request', (request, response) => {
+    const parsedUrl = url.parse(request.url, true);
+    if (request.method === 'GET' && parsedUrl.pathname === '/metadata') {
+        const { id } = parsedUrl.query;
+        const metadata = services.fetchImageMetadata(id);
+        console.log(request.headers)
+    };
+    jsonBody(request, response, (err, body) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            services.createUser(body['userName']);
+        }
+    });
+    response.end('This was served with https!');
+});
+
+server.listen(443);
